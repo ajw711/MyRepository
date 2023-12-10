928c6c816ca523d4826500a87d81e490ebf64cfc
안진원 <@DESKTOP-8TUPEM8>
2023-12-10 14:10:58 +0900

    First commit in dev branch

--- GitHW/src/mygit/MyClass.java
+++ GitHW/src/mygit/MyClass.java
@@ -5,6 +5,10 @@ public class MyClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		
+		System.out.println("Hello1");
+		YourClass y=new YourClass();
+		y.sayHello("123");
 
 	}
 
