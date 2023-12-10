fcd1543beb0fd91e3346c294535b40399fa05e06
안진원 <@DESKTOP-8TUPEM8>
2023-12-10 14:13:25 +0900

    Change in master branch

--- GitHW/src/mygit/YourClass.java
+++ GitHW/src/mygit/YourClass.java
@@ -4,6 +4,8 @@ public class YourClass {
 
 	public void sayHello(String s) {
 		System.out.println("Hello!1 "+s);
+		System.out.println("Hello!12 "+s);
+		System.out.println("Hello!13 "+s);
 	}
 
 }
